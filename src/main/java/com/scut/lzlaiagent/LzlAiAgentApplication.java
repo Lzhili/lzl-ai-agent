4c717e3b18511bd9f084b48051b5157c3bb0dfae
laizhili <969414747@qq.com>
2025-12-03 18:00:04 +0800

    init

---
+++ src/main/java/com/scut/lzlaiagent/LzlAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.scut.lzlaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzlAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzlAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scut/lzlaiagent/LzlAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scut.lzlaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzlAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
