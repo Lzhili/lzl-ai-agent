9475946ed3b31e1290379327cc751a3c27f6a229
laizhili <969414747@qq.com>
2025-12-16 18:28:03 +0800

    5.RAG知识库进阶 - RAG最佳实践：查询重写器

--- src/main/java/com/scut/lzlaiagent/app/LoveApp.java
+++ src/main/java/com/scut/lzlaiagent/app/LoveApp.java
@@ -4,6 +4,7 @@ import com.scut.lzlaiagent.advisor.MyLoggerAdvisor;
 import com.scut.lzlaiagent.advisor.ReReadingAdvisor;
 import com.scut.lzlaiagent.chatMemory.FileBasedChatMemory;
 import com.scut.lzlaiagent.pojo.LoveReport;
+import com.scut.lzlaiagent.rag.QueryRewriter;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
@@ -91,11 +92,16 @@ public class LoveApp {
     @Resource
     private VectorStore pgVectorVectorStore;
 
+    @Resource
+    private QueryRewriter queryRewriter;
+
     // 应用 RAG 知识库进行对话
     public String doChatWithRag(String message, String chatId) {
+        // 查询重写
+        String rewrittenMessage = queryRewriter.doQueryRewrite(message);
         ChatResponse chatResponse = chatClient
                 .prompt()
-                .user(message)
+                .user(rewrittenMessage) // 使用改写后的查询
                 .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
                         .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
                 // 添加日志，便于观察效果
