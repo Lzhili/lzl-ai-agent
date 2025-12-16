6f486bf56ca44115ec863161615eaa6673210255
laizhili <969414747@qq.com>
2025-12-16 19:01:10 +0800

    5.RAG知识库进阶 - RAG最佳实践：多查询扩展

---
+++ src/test/java/com/scut/lzlaiagent/MultiQueryExpanderDemoTest.java
@@ -0,0 +1,23 @@
+package com.scut.lzlaiagent;
+
+import com.scut.lzlaiagent.demo.rag.MultiQueryExpanderDemo;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.rag.Query;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class MultiQueryExpanderDemoTest {
+
+    @Resource
+    private MultiQueryExpanderDemo multiQueryExpanderDemo;
+
+    @Test
+    void test() {
+        List<Query> queries = multiQueryExpanderDemo.expand("如何提升自己的魅力？");
+        Assertions.assertNotNull(queries);
+    }
+}
