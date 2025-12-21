9963ee9cacd7115265de9fe29dcbfd3acd2ec952
laizhili <969414747@qq.com>
2025-12-21 17:01:30 +0800

    7.MCP协议 - MCP 开发实战 - 图片搜索服务 - MCP 服务端开发

---
+++ lzl-image-search-mcp-server/src/test/java/com/scut/lzlimagesearchmcpserver/LzlImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scut.lzlimagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzlImageSearchMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
